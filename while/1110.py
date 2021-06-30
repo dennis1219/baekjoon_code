6f39495eb6473353f5fca64b83c0cfb24679a5b8
dennis1219 <dennis011219@naver.com>
2021-06-30 20:38:55 +0900

    Finish

---
+++ while/1110.py
@@ -0,0 +1,29 @@
+a = int(input())
+z = int(a)
+count = 0
+c = 0
+d = 0
+
+while True:
+	try:
+		if z==0:
+			print("1")
+			break
+		elif c==z:
+			print(count)
+			break
+		elif d==z:
+			print(count)
+			break
+		elif a<10:
+				c=((a%10)*10+a)
+				count=count+1
+				a=int(c)
+		else:
+			d=((a%10)*10+(a//10+a%10)%10)
+			count=count+1
+			a=int(d)
+			
+				
+	except:
+		break
