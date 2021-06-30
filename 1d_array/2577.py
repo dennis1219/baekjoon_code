feccff987f9888cad92e77c5752b99be78dcea8e
dennis1219 <dennis1219@naver.com>
2021-06-30 23:04:30 +0900

    Finish

---
+++ 1d_array/2577.py
@@ -0,0 +1,18 @@
+a = int(input())
+b = int(input())
+c = int(input())
+n = a*b*c
+t = list(map(int, str(n)))
+
+print(t.count(0))
+print(t.count(1))
+print(t.count(2))
+print(t.count(3))
+print(t.count(4))
+print(t.count(5))
+print(t.count(6))
+print(t.count(7))
+print(t.count(8))
+print(t.count(9))
+
+#Need to be used to using loops!
