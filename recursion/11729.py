1d6d2b079403797ea1f2a834e650da2ff02e6cc1
dennis1219 <dennis011219@naver.com>
2021-07-11 16:47:14 +0900

    Create 11729.py

---
+++ recursion/11729.py
@@ -0,0 +1,16 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+def hanoi(n,a,b,c):
+    if n == 1:
+        print(a, c)
+    else:
+        hanoi(n-1,a,c,b)
+        print(a,c)
+        hanoi(n-1,b,a,c)
+
+print(2**n-1)
+hanoi(n,1,2,3)
+
+#Used recursion.
