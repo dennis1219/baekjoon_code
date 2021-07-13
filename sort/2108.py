aaa34408f647dff18b33a7a9e4dfffcdab7ec02c
dennis1219 <dennis011219@naver.com>
2021-07-13 21:51:19 +0900

    Update 2108.py

--- sort/2108.py
+++ sort/2108.py
@@ -7,9 +7,14 @@ l = list()
 for i in range(n):
     l.append(int(sys.stdin.readline()))
 
+l.sort()
+
 mo = statistics.multimode(l)
 
 print(round(statistics.mean(l)))
 print(round(statistics.median(l)))
-print(mo[1])
+if len(mo) == 1:
+    print(mo[0])
+else:
+    print(mo[1])
 print(max(l)-min(l))
