bdf3408561b5a1810efb89049f817a2ef9df8556
dennis1219 <dennis011219@naver.com>
2021-07-14 15:32:14 +0900

    Create 15652.py

---
+++ backtracking/15652.py
@@ -0,0 +1,13 @@
+import sys
+import itertools
+
+n,r = map(int, sys.stdin.readline().split())
+l = list()
+
+for i in range(n):
+    l.append(i+1)
+
+sol = list(itertools.combinations_with_replacement(l , r))
+
+for j in sol:
+    print(*j)
